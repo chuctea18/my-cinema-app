2b32dff4103b7ad11ecb4128258964667ce6bd5f
chuctea18 <thanhtrucpt2011@gmail.com>
2022-05-16 22:29:50 +0700

    add icon

--- src/components/Navbar/MenuItem.jsx
+++ src/components/Navbar/MenuItem.jsx
@@ -1,32 +1,43 @@
+import { MdOutlineLocalMovies, MdOutlineHelp } from 'react-icons/md'
+import { FaCalendarAlt, FaGift, FaTicketAlt } from 'react-icons/fa'
+import { BsInfoCircleFill } from 'react-icons/bs'
+
+
 export const MenuItems = [
     {
         title: 'Phim',
         url: '#',
         cName: 'nav-links',
+        icon: <MdOutlineLocalMovies />
     },
     {
         title: 'Lịch chiếu',
         url: '#',
         cName: 'nav-links',
+        icon: <FaCalendarAlt />
     },
     {
         title: 'Ưu đãi',
         url: '#',
         cName: 'nav-links',
+        icon: <FaGift />
     },
     {
         title: 'Giá vé',
         url: '#',
         cName: 'nav-links',
+        icon: <FaTicketAlt />
     },
     {
         title: 'Giới thiệu',
         url: '#',
         cName: 'nav-links',
+        icon: <BsInfoCircleFill />
     },
     {
         title: 'Hỗ trợ',
         url: '#',
         cName: 'nav-links',
+        icon: <MdOutlineHelp />
     },
 ]
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import { useRef, useEffect, useCallback, useState } from "react";
 import { MenuItems } from "./MenuItem";
 import "./navbar.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col } from "react-bootstrap";
+
 
 function Navbar() {
   const navRef = useRef(null);
@@ -49,12 +49,14 @@ function Navbar() {
         <div className="container-fluid">
           <div className="row">
             <div className="col-3 d-flex align-items-center">
-              <img
+              <h1 ref={logoRef}
+                className="navbar-logo"> Lotto Cinema</h1>
+              {/* <img
                 ref={logoRef}
                 className="navbar-logo"
                 src={`${process.env.PUBLIC_URL}/a1.svg`}
                 alt="logo"
-              />
+              /> */}
             </div>
             <div className="col-9">
               <div className="d-flex justify-content-end my-3">
@@ -75,15 +77,16 @@ function Navbar() {
                     <path d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z" />
                   </svg>
                 </form>
-
                 <button className="btn btn-cinema mx-2">đăng nhập</button>
                 <button className="btn btn-cinema mx-2">đăng ký</button>
+
               </div>
               <ul className="nav-menu">
                 {MenuItems.map((item, index) => {
                   return (
                     <li key={index}>
                       <a className={item.cName} href={item.url}>
+                        <span>{item.icon}</span>
                         {item.title}
                       </a>
                     </li>
@@ -93,31 +96,6 @@ function Navbar() {
             </div>
           </div>
         </div>
-        {/* <Container>
-          <Row>
-            <Col>
-              <h1 className="navbar-logo">Lotto Cinema</h1>
-            </Col>
-            <Col>
-              <div className="d-flex justify-content-end my-3">
-                <form className="d-flex">
-                  <input
-                    className="form-control me-2"
-                    type="search"
-                    placeholder="Search"
-                    aria-label="Search"
-                  />
-                  <button className="btn btn-outline-success" type="submit">
-                    Search
-                  </button>
-                </form>
-
-                <button>dang nhap</button>
-                <button>dang nhap</button>
-              </div>
-            </Col>
-          </Row>
-        </Container> */}
       </nav>
     </div>
   );
